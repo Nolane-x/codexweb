e13367c3301c6e0fb2dc60dee337b7511a88c33b
Nolane-x <casioreview20@gmail.com>
2026-08-14 11:15:38 +0700

    feat: mount user-controlled update prompt

--- launcher/src/main.tsx
+++ launcher/src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { CouncilDock } from "./CouncilDock";
 import { CouncilSetupPanel } from "./CouncilSetupPanel";
+import { CouncilUpdatePrompt } from "./CouncilUpdatePrompt";
 import "./tokens.css";
 import "./styles.css";
 import "./council.css";
 import "./council-setup.css";
+import "./council-update.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <CouncilDock />
     <CouncilSetupPanel />
+    <CouncilUpdatePrompt />
   </React.StrictMode>,
 );
